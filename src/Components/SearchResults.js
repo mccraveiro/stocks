b66bea228d904f53c21d2a770c1eb10289bfb7fa
Mateus Craveiro <mccraveiro@gmail.com>
2018-12-17 01:56:38 -0200

    Create components directory

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { css, jsx } from '@emotion/core';
-import settings from './settings.json';
+import settings from '../settings.json';
 
 const listStyle = css`
   list-style: none;
