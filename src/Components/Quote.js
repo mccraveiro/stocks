df80bbd049df0718e33599e46f604a496e49e773
Mateus Craveiro <mccraveiro@gmail.com>
2018-12-17 14:43:27 -0200

    Add timeseries chart

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -2,6 +2,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { css, jsx } from '@emotion/core';
+import { VictoryGroup, VictoryArea, VictoryContainer } from 'victory';
+
 import settings from '../settings.json';
 
 const { baseURL, APIKey } = settings
@@ -10,46 +12,77 @@ const quoteStyle = css`
   border: 1px solid #ececec;
   margin: 8px;
   width: 250px;
-  height: 150px;
-  padding: 20px;
+  height: 277px;
+  padding: 0;
   text-align: left;
+  position: relative;
 `
 
 const headerStyle = css`
-  padding: 0;
+  padding: 20px;
   margin: 0;
+`
+
+const priceStyle = css`
+  position: absolute;
+  bottom: 20px;
+  width: 100%;
+  text-align: center;
+  z-index: 1;
+`
+
+const changeStyle = css`
+  position: absolute;
+  font-size: 1em;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+  z-index: 1;
 `
 
 function getChangeClassName (change) {
   if (!change) return
 
-  const changeWithoutPercentage = Number(change.slice(0, -1))
-
-  if (changeWithoutPercentage < 0) {
+  if (change < 0) {
     return css`
+      ${changeStyle};
       color: red;
     `
   }
 
   return css`
+    ${changeStyle};
     color: green;
   `
 }
 
 function Quote ({ symbol }) {
-  const [price, setPrice] = useState('-');
-  const [change, setChange] = useState('-');
+  const [price, setPrice] = useState(0);
+  const [change, setChange] = useState(0);
+  const [timeseries, setTimeseries] = useState([]);
 
   async function getPrice () {
-    const result = await axios(`${baseURL}?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${APIKey}`);
+    const result = await axios(`${baseURL}?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${APIKey}`);
     console.log(result)
 
-    if (!result.data['Global Quote']) {
+    if (!result.data['Time Series (Daily)']) {
       return
     }
 
-    setPrice(result.data['Global Quote']['05. price'])
-    setChange(result.data['Global Quote']['10. change percent'])
+    const data = Object.entries(result.data['Time Series (Daily)'])
+      .filter(([label, data]) => Number(data['5. volume']) !== 0)
+      .map(([label, data]) => ({ x: label, y: Number(data['4. close'])}))
+      .reverse()
+
+    console.log(data)
+
+    const firstPrice = data[0].y;
+    const lastPrice = data[data.length - 1].y;
+    const currentChange = ((lastPrice / firstPrice) - 1) * 100;
+
+    setPrice(lastPrice);
+    setChange(currentChange);
+    setTimeseries(data)
   }
 
   useEffect(() => { getPrice() }, [symbol]);
@@ -57,8 +90,27 @@ function Quote ({ symbol }) {
   return (
     <div css={quoteStyle}>
       <h1 css={headerStyle}>{ symbol }</h1>
-      <h2>{ price }</h2>
-      <h2 css={getChangeClassName(change)}>{ change }</h2>
+      <h2 css={priceStyle}>{ price.toFixed(2) }</h2>
+      <h2 css={getChangeClassName(change)}>{ change.toFixed(2) }%</h2>
+      <VictoryGroup
+        padding={0}
+        width={250}
+        height={200}
+        containerComponent={<VictoryContainer responsive={false} />}
+      >
+        <VictoryArea
+          interpolation="natural"
+          data={timeseries}
+          style={{
+            data: {
+              fill: change >= 0 ? 'green' : 'red',
+              fillOpacity: 0.4,
+              stroke: change >= 0 ? 'green' : 'red',
+              strokeWidth: 2,
+            },
+          }}
+        />
+      </VictoryGroup>
     </div>
   );
 }
